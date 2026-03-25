3e17be01bb0a72e928cc6a493981c9b4867e466f
VinaPotenztech <vina@potenztech.com>
2026-03-25 19:22:09 +0530

    remove unused code

--- routes/aiRoutes.js
+++ routes/aiRoutes.js
@@ -5,7 +5,6 @@ import {
   history,
   exportPdf,
   exportDocx,
-  streamGenerate,
 } from "../controllers/aiController.js";
 
 const router = Router();
@@ -14,6 +13,5 @@ router.post("/generate", auth, generate);
 router.get("/history", auth, history);
 router.get("/export/:id/pdf", auth, exportPdf);
 router.get("/export/:id/docx", auth, exportDocx);
-router.get("/stream", auth, streamGenerate);
 
 export default router;
